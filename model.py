5664131dd1782092ac8eabdc47d9bfbc48073d3b
Ramyar Zarza <ramyar.zarza@gmail.com>
2025-06-21 20:15:21 -0230

    Add debug print statements

--- model.py
+++ model.py
@@ -51,8 +51,12 @@ def detect_tennis_balls_from_webcam():
         if not ret:
             break
 
+        print("=== detect_color yellow called! ===")
         frame, (x_y, y_y), area_y = detect_color(frame, [20, 80, 80], [55, 255, 255], (0, 255, 255))  #yellow
+        print("=== detect_color yellow finished! ===")
+        print("=== detect_color green called! ===")
         frame, (x_o, y_o), area_o = detect_color(frame, [35, 50, 50], [85, 255, 255], (0, 255, 0))  #green
+        print("=== detect_color green finished! ===")
 
         print("--------------------------------------------------")
         print(f"Yellow Ball - Center: ({x_y}, {y_y}), Area: {area_y}")
@@ -68,23 +72,36 @@ def detect_tennis_balls_from_webcam():
         
         if x == -1:
             print("No ball detected.")
+            
+            print("=== MoveRight called!")
             moveRight()
+            print("=== MoveRight finished!")
+            
             continue
         if 720/ 2 - 50 < x < 720 / 2 + 50:
             if area_y > 30000 and arm_up:
                 arm_up = False
+                print("=== arm close called!")
                 close() # Close the arm
+                print("=== arm close finished!")
                 sleep(2)  # Simulate arm movement delay
             elif area_o > 30000 and not arm_up:
                 arm_up = True
+                print("=== arm open called!")
                 open() # Open the arm
+                print("=== arm open finished!")
                 sleep(2)  # Simulate arm movement delay
-            moveForward() 
-            pass
+            print("=== MoveForward called!")
+            moveForward()
+            print("=== MoveForward finished!")
         elif x <= 720 / 2 - 50:
-            moveLeft() 
+            print("=== MoveLeft called!")
+            moveLeft()
+            print("=== MoveLeft finished!")
         else:
+            print("=== MoveRight called!")
             moveRight()
+            print("=== MoveRight finished!")
 
 
         # Quitting program with 'q'
